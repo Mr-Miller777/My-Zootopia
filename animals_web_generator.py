6dc39fea59179594706f1919a9e8e56ecdfeb75e
Viacheslav Miller <vaceslav_miller@mail.ru>
2026-05-18 00:41:07 +0200

    Tierdaten in HTML einfügen

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,4 +1,5 @@
 import json
+from os import write
 
 
 def load_data(file_path):
@@ -15,25 +16,47 @@ def get_animal_data(animals_data):
     - The first location from the list of locations
     - Type
     """
+    output = ""  # define an empty string
     for animal in animals_data:
+        # append information to each string
         name = animal.get("name")
         diet = animal["characteristics"].get("diet")
         location = animal["locations"][0]
         animal_type = animal["characteristics"].get("type")
         if name is not None:
-            print(f"Name: {name}")
+            output += f"Name: {name}\n"
         if diet is not None:
-            print(f"Diet: {diet}")
+            output += f"Diet: {diet}\n"
         if location is not None:
-            print(f"Lokation: {location}")
+            output += f"Lokation: {location}\n"
         if animal_type is not None:
-            print(f"Type: {animal_type}")
-        print("") #Empty print for spacing
+            output += f"Type: {animal_type}\n"
+        output += "\n"  # Empty print for spacing
+    print(output)
+    return output
+
+def read_template(template_path):
+    with open(template_path, "r") as fileobj:
+        template = fileobj.read()
+        return template
+
+def add_animals_to_template(template, output):
+    animals_text = template.replace("__REPLACE_ANIMALS_INFO__", output)
+    return animals_text
+
+def write_animals_file(animals_text):
+    with open("animals.html", "w") as fileobj:
+        fileobj.write(animals_text)
 
 
 def main():
     animals_data = load_data('animals_data.json')
-    get_animal_data(animals_data)
+    output = get_animal_data(animals_data)
+    template = read_template('animals_template.html')
+    animals_text = add_animals_to_template(template, output)
+    write_animals_file(animals_text)
+
+
 
 
 if __name__ == '__main__':
